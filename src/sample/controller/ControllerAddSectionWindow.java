f7762d28c1a12e19956accd311bdcef9a200deed
KlevcevichAV <alexandr.klevcevich@gmail.com>
2020-08-15 17:02:41 +0300

    feat(add-section-window): first window design created and his controller

---
+++ src/sample/controller/ControllerAddSectionWindow.java
@@ -0,0 +1,15 @@
+package sample.controller;
+
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class ControllerAddSectionWindow {
+
+    @FXML
+    private Button addButton;
+
+    @FXML
+    private Button cancelButton;
+
+}
