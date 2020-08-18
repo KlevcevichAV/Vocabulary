d81ec936cb3477b8e64d8993da45b9d81c7c6b45
KlevcevichAV <alexandr.klevcevich@gmail.com>
2020-08-18 17:43:12 +0300

    style(Constant): update Constant.java

--- src/sample/Constant.java
+++ src/sample/Constant.java
@@ -1,8 +1,33 @@
 package sample;
 
 public class Constant {
-    public static final String WORD_IN_ENGLISH = "Английский";
-    public static final String WORD_IN_RUSSIAN = "Русский";
-    public static final String WORD_IN_ENGLISH_EN = "wordInEn";
-    public static final String WORD_IN_RUSSIAN_EN = "wordInRu";
+    public static final String WORD_IN_ENGLISH = "wordInEn";
+    public static final String WORD_IN_RUSSIAN = "wordInRu";
+    public static final String SEMICOLON = ";";
+    public static final String LEFT_PARENTHESIS = "(";
+    public static final String RIGHT_PARENTHESIS = ");";
+    public static final String NAME_SECTION = "section";
+    public static final String SECTION = "Section";
+    public static final String ENGLISH = "English";
+    public static final String RUSSIAN = "Russian";
+    public static final String ID = "id";
+    public static final String COMMA = " , ";
+    public static final String ALL_WORLDS_USER = "All worlds";
+    public static final String ALL_WORLDS_TABLE = "all_worlds";
+    public static final String CREATE_TABLE = "create table";
+    public static final String INSERT = "insert";
+    public static final String VALUES = "values(";
+    public static final String AND = " and ";
+    public static final String WHERE = " where ";
+    public static final String DELETE_FROM = "delete from";
+    public static final String WORD_IN_RU = "wordInRu=";
+    public static final String WORD_IN_EN = "wordInEn=";
+    public static final String SECTION_ = "section=";
+    public static final String SELECT_FROM = "select * from";
+    public static final String VALUES_CREATE_TABLE_SELECTIONS = "(" +
+            "id int auto_increment primary key ," +
+            "wordInEn varchar(30)," +
+            "wordInRu varchar(30)" +
+            ");";
+
 }
